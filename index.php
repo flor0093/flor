127a67e558f0932d7525681da65ba289bf53fa73
Danny Monte <67159240+diirenek2@users.noreply.github.com>
2021-12-28 20:47:47 -0300

    imagenes

--- index.php
+++ index.php
@@ -4,9 +4,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=8.0">
-    <title>proyectonaco</title>
-</head>
-<body>
-    
-</body>
+    <title>Chikistrikis</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></head>
+    <body>
+        <div class="container">
+            <div class="row bg bg-info">
+                <div class="col">
+                    <img src="https://media.gq.com.mx/photos/5f5a43c04464f9b88fb2681d/master/pass/dune.jpg" class="img-fluid" title="dune">
+                </div>
+                <div class="col">
+                    <img src="https://as01.epimg.net/meristation/imagenes/2021/10/27/noticias/1635321363_017223_1635321430_noticia_normal.jpg" class="img-fluid" alt="...">
+                </div>
+                <div class="col">
+                    <img src="https://as01.epimg.net/meristation/imagenes/2021/10/27/noticias/1635321363_017223_1635321430_noticia_normal.jpg" class="img-fluid" alt="...">
+                </div>
+                <div class="col">
+                    <img src="https://as01.epimg.net/meristation/imagenes/2021/10/27/noticias/1635321363_017223_1635321430_noticia_normal.jpg" class="img-fluid" alt="...">
+                </div>
+                <div class="col">
+                    <img src="https://as01.epimg.net/meristation/imagenes/2021/10/27/noticias/1635321363_017223_1635321430_noticia_normal.jpg" class="img-fluid" alt="...">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <img src="https://as01.epimg.net/meristation/imagenes/2021/10/27/noticias/1635321363_017223_1635321430_noticia_normal.jpg" class="img-fluid" alt="...">
+                </div>
+                <div class="col">
+                    <img src="https://as01.epimg.net/meristation/imagenes/2021/10/27/noticias/1635321363_017223_1635321430_noticia_normal.jpg" class="img-fluid" alt="...">
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
