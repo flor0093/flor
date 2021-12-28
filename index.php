d72be732c8fa4f301ba859f4797aa212e2496434
Danny Monte <67159240+diirenek2@users.noreply.github.com>
2021-12-28 20:25:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-chikis naka
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=8.0">
+    <title>proyectonaco</title>
+</head>
+<body>
+    
+</body>
+</html>
